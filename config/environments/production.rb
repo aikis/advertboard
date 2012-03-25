b34a1a7c12cea3753a1e0437eb1865ef323fadf3
Maxim <aikismax@gmail.com>
2012-03-26 00:03:50 +0300

    Config mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,23 @@ Testapp::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # To use devise mailer
+  config.action_mailer.default_url_options = { :host => 'http://advertboard.heroku.com/' }
+  require 'tlsmail'    
+  Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
+
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.perform_deliveries = true
+  ActionMailer::Base.raise_delivery_errors = true
+  ActionMailer::Base.smtp_settings = {
+    :enable_starttls_auto => true,  
+    :address            => 'smtp.gmail.com',
+    :port               => 587,
+    :tls                  => true,
+    :domain             => 'gmail.com',
+    :authentication     => :plain,
+    :user_name          => 'aikismax@gmail.com',
+    :password           => 'Aikis13151725#@&#)%@'
+  }
 end
