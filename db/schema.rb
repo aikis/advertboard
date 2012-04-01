2eb352339bff84c39c7c84fe9597875dd9cd2258
Maxim <aikismax@gmail.com>
2012-04-02 01:19:15 +0300

    Adding admin by seed data

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120328220741) do
+ActiveRecord::Schema.define(:version => 20120330102821) do
 
   create_table "adverts", :force => true do |t|
     t.text     "text"
@@ -31,6 +31,19 @@ ActiveRecord::Schema.define(:version => 20120328220741) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  create_table "rails_admin_histories", :force => true do |t|
+    t.text     "message"
+    t.string   "username"
+    t.integer  "item"
+    t.string   "table"
+    t.integer  "month",      :limit => 2
+    t.integer  "year",       :limit => 5
+    t.datetime "created_at",              :null => false
+    t.datetime "updated_at",              :null => false
+  end
+
+  add_index "rails_admin_histories", ["item", "table", "month", "year"], :name => "index_rails_admin_histories"
 
   create_table "users", :force => true do |t|
     t.string   "login"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +6,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(login: 'root', full_name: 'Администратор', birthday: '2000-01-01',
+  address: 'Независимости', city: 'Минск', state: 'Минск', country: 'Беларусь', zip: '220000',
+  email: 'roo@root.com', password: 'root123456', role: 'admin')
