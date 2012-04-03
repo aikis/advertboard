a052328250521663b60f487974f11a12d813aaf6
Maxim <aikismax@gmail.com>
2012-04-03 23:48:00 +0300

    add pretty shorthing text for index page

--- app/models/advert.rb
+++ app/models/advert.rb
@@ -7,4 +7,13 @@ class Advert < ActiveRecord::Base
                     :default_url => "/system/images/default_image.jpg", :default_style => :thumb
   attr_accessor :delete_image
   before_validation { self.image.clear if self.delete_image == '1' }
+
+  def thumb_text
+    if self.text.size > 160
+      thumb = self.text[0..159]
+      thumb = thumb[0..(thumb.rindex(/\W/)-1)] + "..."
+    else
+      self.text
+    end
+  end
 end
